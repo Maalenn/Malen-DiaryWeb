ef32628feb91ef53229d1e8a4881923611d7d04c
Malen <malen8622@gmail.com>
2024-06-30 15:02:53 +0700

    feat(homePage): add entry component

---
+++ src/js/component/frompopup-entry-component.js
@@ -0,0 +1,41 @@
+// Create entry and form popup component
+const FormPopupEntryContainer = () => {
+    return `
+        <section class="main-entry">
+            <div id="entries-container"></div>
+        </section>
+        <section id="form-popup" class="main-form">
+            <form id="diary-form">
+                <section class="header1">
+                    <nav class="navigation-bar container-max-width">
+                        <div class="navigation-left">
+                            <div class="logo">
+                                <img src="/khunmalen-diary-web/src/image/logo.png" alt="Logo" />
+                            </div>
+                            <h3>Diary Website Application</h3>
+                        </div>
+                    </nav>
+                </section>
+                <section class="info-container container-max-width">
+                    <input type="date" id="date" required />
+                    <input type="text" id="title" placeholder="Untitled....." required />
+                    <textarea id="content" placeholder="Write something here......"required></textarea>
+                    <div class="btn-container-popup">
+                        <button type="submit" id="submit-button">Create</button>
+                        <button type="button" id="cancel-button">Cancel</button>
+                    </div>
+                </section>
+                
+            </form>
+        </section>
+      `;
+  };
+  
+  class FormPopupEntry extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = FormPopupEntryContainer();
+    }
+  }
+  
+  customElements.define("formpopup-entry-component", FormPopupEntry);
+  
--- src/js/main.js
+++ src/js/main.js
@@ -1,140 +1,18 @@
 function openNavBar() {
-    // Add class openSideBar to side-bar
-    document.querySelector(".side-bar").classList.add("open-sideBar");
-    // Make what is behind sidebar non-scrollable
-    document.querySelector("html").classList.add("overflow-y-hidden");
-    // Make what is behind sidebar blur
-    document.querySelector(".background-blur").classList.add("bg-blur");
-  }
-  
-  function closeNavBar() {
-    // Remove class openSideBar from side-bar
-    document.querySelector(".side-bar").classList.remove("open-sideBar");
-    // Allow scrolling
-    document.querySelector("html").classList.remove("overflow-y-hidden");
-    // Remove blur from background
-    document.querySelector(".background-blur").classList.remove("bg-blur");
-  }
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("diary-form");
-    const entriesContainer = document.getElementById("entries-container");
-    const formPopup = document.getElementById("form-popup");
-    const openFormButton = document.getElementById("open-form-button");
-    const cancelButton = document.getElementById("cancel-button");
-    let entries = [];
-  
-    openFormButton.addEventListener("click", () => {
-      formPopup.classList.add("show");
-      // Make what is behind form non-scrollable
-      document.querySelector("html").classList.add("overflow-y-hidden");
-      // Make what is behind form blur
-      document.querySelector(".background-blur").classList.add("bg-blur");
-    });
-  
-    cancelButton.addEventListener("click", () => {
-      formPopup.classList.remove("show");
-      form.reset();
-      form.dataset.mode = "";
-      form.dataset.index = "";
-      // Allow scrolling
-      document.querySelector("html").classList.add("overflow-y-hidden");
-      // Remove blur from background
-      document.querySelector(".background-blur").classList.add("bg-blur");
-    });
-  
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const title = document.getElementById("title").value;
-      const content = document.getElementById("content").value;
-      const date = document.getElementById("date").value;
-  
-      if (title && content && date) {
-        const entry = { title, content, date };
-  
-        if (form.dataset.mode === "edit") {
-          const index = parseInt(form.dataset.index);
-          entries[index] = entry;
-          form.dataset.mode = "";
-          form.dataset.index = "";
-        } else {
-          entries.push(entry);
-        }
-  
-        saveEntries();
-        displayEntries();
-        form.reset();
-        formPopup.classList.remove("show");
-        // Allow scrolling
-        document.querySelector("html").classList.add("overflow-y-hidden");
-        // Remove blur from background
-        document.querySelector(".background-blur").classList.add("bg-blur");
-      } else {
-        alert("Please fill in all fields.");
-      }
-    });
-  
-    const saveEntries = () => {
-      localStorage.setItem("diary-entries", JSON.stringify(entries));
-    };
-  
-    const loadEntries = () => {
-      const storedEntries = localStorage.getItem("diary-entries");
-      entries = storedEntries ? JSON.parse(storedEntries) : [];
-    };
-  
-    const displayEntries = () => {
-      entriesContainer.innerHTML = "";
-  
-      entries.forEach((entry, index) => {
-        const entryDiv = document.createElement("div");
-        entryDiv.classList.add("entry");
-        entryDiv.innerHTML = `
-          <section class="entry-to-container"> 
-              <div> Date: ${entry.date} </div>
-              <hr>
-              <section class="btn-edit-delete-container">
-                  <button onclick="editEntry(${index})">
-                      <span class="material-symbols-outlined edit-btn">edit</span></button>
-                  <button onclick="deleteEntry(${index})">
-                      <span class="material-symbols-outlined delete-btn">delete</span>
-                  </button> 
-              </section>
-               <div>
-              <p>${entry.content}</p> 
-          </div>
-          </section>
-          <div class="entry-title">
-             <hr> 
-              <h3>${entry.title}</h3>
-          </div>
-        `;
-        entriesContainer.appendChild(entryDiv);
-      });
-    };
-  
-    window.editEntry = (index) => {
-      const entry = entries[index];
-      document.getElementById("title").value = entry.title;
-      document.getElementById("content").value = entry.content;
-      document.getElementById("date").value = entry.date;
-      formPopup.classList.add("show");
-      form.dataset.mode = "edit";
-      form.dataset.index = index;
-      // Make what is behind form non-scrollable
-      document.querySelector("html").classList.add("overflow-y-hidden");
-      // Make what is behind form blur
-      document.querySelector(".background-blur").classList.add("bg-blur");
-    };
-  
-    window.deleteEntry = (index) => {
-      entries.splice(index, 1);
-      saveEntries();
-      displayEntries();
-    };
-  
-    // Initial load from localStorage
-    loadEntries();
-    displayEntries();
-  });
-  
+  // Add class openSideBar to side-bar
+  document.querySelector(".side-bar").classList.add("open-sideBar");
+  // Make what is behind sidebar non-scrollable
+  document.querySelector("html").classList.remove("overflow-y-hidden");
+  // Make what is behind sidebar blur
+  document.querySelector(".background-blur").classList.remove("bg-blur");
+}
+
+function closeNavBar() {
+  // Remove class openSideBar from side-bar
+  document.querySelector(".side-bar").classList.remove("open-sideBar");
+  // Allow scrolling
+  document.querySelector("html").classList.add("overflow-y-hidden");
+  // Remove blur from background
+  document.querySelector(".background-blur").classList.add("bg-blur");
+}
+
