dd1df4f826b65961e9da9503b7a06dd60866716b
Malen <malen8622@gmail.com>
2024-06-30 16:49:12 +0700

    feat(homePage): add footer

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ function openNavBar() {
   // Add class openSideBar to side-bar
   document.querySelector(".side-bar").classList.add("open-sideBar");
   // Make what is behind sidebar non-scrollable
-  document.querySelector("html").classList.remove("overflow-y-hidden");
+  document.querySelector("html").classList.add("overflow-y-hidden");
   // Make what is behind sidebar blur
   document.querySelector(".background-blur").classList.remove("bg-blur");
 }
@@ -11,7 +11,7 @@ function closeNavBar() {
   // Remove class openSideBar from side-bar
   document.querySelector(".side-bar").classList.remove("open-sideBar");
   // Allow scrolling
-  document.querySelector("html").classList.add("overflow-y-hidden");
+  document.querySelector("html").classList.remove("overflow-y-hidden");
   // Remove blur from background
   document.querySelector(".background-blur").classList.add("bg-blur");
 }
